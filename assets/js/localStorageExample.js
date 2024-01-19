dd1c078e01f522b6d3e920743356c1c2bede4c1c
GabrielIkpolo <contact.gikps@gmail.com>
2024-01-19 06:48:46 +0100

    implimented getting input elements

--- assets/js/localStorageExample.js
+++ assets/js/localStorageExample.js
@@ -20,7 +20,42 @@ localStorage.removeItem('isLoggedIn');
 
 
 /**
+ * 
+ * 
+ * 
+ * 
+Introduction:
+localStorage is a web storage API in JavaScript that allows web 
+applications to store data persistently in a user's browser.
+Unlike session storage, which is limited to a session, localStorage 
+retains data even when the browser is closed and reopened. It is 
+a key-value storage mechanism, where data is stored as strings.
 LocalStorage can be used to store  
+
+localStorage stores data as strings. If you want to store objects or
+arrays, you need to serialize and deserialize them using JSON.stringify
+and JSON.parse, respectively.
+
+Eg:
+
+const userData = { name: 'John', age: 25 };
+localStorage.setItem('user', JSON.stringify(userData));
+
+const storedUser = JSON.parse(localStorage.getItem('user'));
+
+
+Capacity:
+localStorage has a default storage limit of around 5 MB per domain. 
+This is generally sufficient for storing small amounts of data, such 
+as user preferences or authentication tokens.
+
+Use Cases:
+User Preferences: Storing user-specific settings and preferences.
+Caching: Saving API responses or frequently used data to reduce server requests.
+Authentication: Storing tokens or user information for persistent logins
+
+
+
 
 Strings: You can store plain text or serialized data as strings.
 
